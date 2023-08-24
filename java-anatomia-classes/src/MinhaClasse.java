722a8b4655982b3a2147baeaf729b07300f0462e
Patrícia Gheller <121643390+PatriciaGheller@users.noreply.github.com>
2023-08-24 20:43:38 -0300

    Adiciona metodos, parametros e variaveis

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,14 @@
  public class MinhaClasse {
   public static void main (String [] args) {
 
-    System.out.println("Olá turma, sejam bem-vindos");
+    String primeiroNome = "Patrícia";
+    String segundoNome="Gheller";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
   }
-}
+  
+   public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do método " + primeiroNome.concat("").concat(segundoNome);
+   }
+  }
