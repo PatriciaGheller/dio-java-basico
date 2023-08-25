8d961f75054397e8eacd6370044b57afd90e4edb
Patrícia Gheller <121643390+PatriciaGheller@users.noreply.github.com>
2023-08-25 20:29:39 -0300

    Adiciona identação

--- java-anatomia-classes/src/BoletimEstudantil.java
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -1,11 +1,11 @@
   public class BoletimEstudantil {
       public static void main(String[] args) {
-      int mediaFinal = 6;
-      if(mediaFinal<6)
-      System.out.println("REROVADO");
-      else if(mediaFinal==6)
-      System.out.println("PROVA MINERVA");
-      else
-      System.out.println("APROVADO");
+           int mediaFinal = 6;
+           if (mediaFinal<6)
+               System.out.println("REROVADO");
+           else if(mediaFinal==6)
+               System.out.println("PROVA MINERVA");
+           else
+               System.out.println("APROVADO");
     }
   }
